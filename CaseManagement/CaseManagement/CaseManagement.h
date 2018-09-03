f7f9dffc7df2d445ea45812fc6a68b08d2cc541d
Shelock <Farhad.public@gmail.com>
2018-09-03 09:56:57 +0430

    create project

---
+++ CaseManagement/CaseManagement/CaseManagement.h
@@ -0,0 +1,19 @@
+//
+//  CaseManagement.h
+//  CaseManagement
+//
+//  Created by Farhad Faramarzi on 9/3/18.
+//  Copyright © 2018 Golrangsys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CaseManagement.
+FOUNDATION_EXPORT double CaseManagementVersionNumber;
+
+//! Project version string for CaseManagement.
+FOUNDATION_EXPORT const unsigned char CaseManagementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaseManagement/PublicHeader.h>
+
+
